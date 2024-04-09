c41a44bd6d9c3ed35b646bbade8f33ff8fbdb157
wendesongomes <wendeson.gomes@live.com>
2024-04-09 13:36:14 -0300

    change revalidate local

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/header/header'
+import { revalidateTag } from 'next/cache'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  revalidateTag('portfolio')
   return (
     <html lang="en" className="scroll-smooth">
       <body
